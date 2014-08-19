c0a1ff6ddcaf254f5420a86d2ef6647b19f48aa4
Jorge Yau <jyau01@citymail.cuny.edu>
2014-08-19 13:26:33 -0400

    Starting window texture

--- app/assets/js/city/ObsidianCityBuilding.js
+++ app/assets/js/city/ObsidianCityBuilding.js
@@ -7,8 +7,43 @@
 /*******************************
  * ObsidianCity Window Texture *
  *******************************/
-ObsidianCity.prototype.windowTexture = function() {
+ObsidianCity.prototype.textureCanvas = function(width, height) {
+  // Create canvas and set dimensions
+  var canvas = document.createElement('canvas');
+  canvas.width = width || 32;
+  canvas.height = height || 32;
 
+  // Paint in canvas
+  var ctx = canvas.getContext('2d');
+  ctx.fillStyle = '#FFFFFF';
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  return ctx;
+};
+
+
+ObsidianCity.prototype.createTextureMaterial = function(canvas) {
+  var texture = new THREE.Texture(canvas);
+  return new THREE.MeshLambertMaterial({
+    map: texture,
+    vertexColors: THREE.VertexColors,
+  });
+};
+
+
+ObsidianCity.prototype.windowStripeMaterial = function() {
+  var width = 32;
+  var height = 64;
+  var ctx = this.textureCanvas(width, height);
+
+  // Draw in random shade in windows
+  for (var y=2; y<height; y += 2) {
+    for (var x=0; x<width; x += 2) {
+      var value = Math.floor( Math.random() * height );
+      ctx.fillStyle = 'rgb(' + [value, value, value].join(',') + ')';
+      ctx.fillRect(x, y, 2, 1);
+    }
+  }
+  return this.createTextureMaterial(ctx.canvas);
 };
 
 
@@ -48,7 +83,8 @@ ObsidianCity.prototype.generateBuilding = function(type, width, length) {
 ObsidianCity.prototype.blockBuilding = function(width, length) {
   var height = this.utils.randomInteger(30, 50);
   var geometry = this.geometry.box;
-  var material = this.material.basicLambert;
+  // [TODO] resuable texture
+  var material = this.windowStripeMaterial();
   var mesh = new THREE.Mesh(geometry, material);
   mesh.scale.set(width, height, length);
   return mesh;
